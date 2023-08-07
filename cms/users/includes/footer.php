d8bdb36cb2283b9e80dc5aa4ef124ffdfd09a7ef
Rishabh Mishra <mishrarm955964@gmail.com>
2023-08-07 21:25:41 +0530

    College_project

---
+++ cms/users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+     <div class="text-center">
+         Developed By SRIMT Student|Lucknow
+         <a href="#" class="go-top">
+             <i class="fa fa-angle-up"></i>
+         </a>
+     </div>
+ </footer>
